240e6af1fc9eb341e78752f724c90c1c9bcdc1b9
Michał Dolny <michal.dolny@gmail.com>
2017-04-10 17:31:39 +0200

    przykladowe rozwiazanie

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
  */
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
-var countriesDetailList = $('#countries_details');
 
 $('#search').click(searchCountries);
 
@@ -19,14 +18,16 @@ function searchCountries() {
 
 function showCountriesList(resp) {
     countriesList.empty();
-    countriesDetailList.empty();
     resp.forEach(function(item){
-        $('<li>').text('Nazwa Kraju: ' + item.name).appendTo(countriesList);
-        $('<ul><li>').text('Język: ' + item.languages).appendTo(countriesList);
-        $('<ul><li>').text('Populacja: ' + item.population).appendTo(countriesList);
-        $('<ul><li>').text('Region: ' + item.region).appendTo(countriesList);
-        $('<ul><li>').text('Time zone: ' + item.timezones).appendTo(countriesList);
-        $('<ul><li>').text('________________________________________________').appendTo(countriesList);
+        var singleCountry = $('<li>' + item.name + '</li>');
 
+        var countryDetails = $('<ul class="country-details"></ul>');
+        countryDetails.append('<li>Język: ' + item.languages + '</li>');
+        countryDetails.append('<li>Populacja: ' + item.population + '</li>');
+        countryDetails.append('<li>Region: ' + item.region + '</li>');
+        countryDetails.append('<li>Time zone: ' + item.timezones + '</li>');
+
+        countryDetails.appendTo(singleCountry);
+        singleCountry.appendTo(countriesList);
     });
 };
